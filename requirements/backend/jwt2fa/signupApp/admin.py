9d755bf183e609ec7a6622876feb46946128a090
Noxe42 <amineouahab63@gmail.com>
2025-03-02 03:55:52 +0100

    recuperation du projet fini

--- requirements/backend/jwt2fa/signupApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import SignupUser
-
-# Register your models here.
-
-@admin.register(SignupUser)
-class SignupUserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'username', 'is_staff')
-    search_fields = ('email', 'username')
